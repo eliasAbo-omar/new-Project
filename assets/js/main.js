e44220c061c38a0dba55019a19fbf56c442ab6d2
Elias <ekasa123@gmail.com>
2026-04-01 01:46:53 +0300

    log-in

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+let firstInput = document.getElementById("first-input");
+let scondInput = document.getElementById("scond-input");
+let spanText = document.createElement("span");
+const form = document.querySelector(".log-in");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const nameValue = firstInput.value.toLowerCase();
+  const groupValue = scondInput.value.toLowerCase();
+
+  if (nameValue === "" || groupValue === "") {
+    spanText.textContent = "الرجاء تعبئة جميع الخانات";
+    spanText.style.color = "red";
+    form.appendChild(spanText);
+  } else if (nameValue !== "" && groupValue !== "") {
+    spanText.textContent = "";
+    localStorage.setItem("na", nameValue);
+    localStorage.setItem("gr", groupValue);
+  }
+
+  window.location.href = "";
+});
